450033d907d0e6180310b4f7e40d7443f47abddb
dat98it <dat98.it@gmail.com>
2024-08-07 09:59:28 +0700

    feat: UI NewPassword screen

---
+++ src/screens/NewPassword/index.tsx
@@ -0,0 +1,68 @@
+import {Input, Text, TouchableOpacity, View} from '@components/index';
+import React from 'react';
+const NewPassword: React.FC = () => {
+  return (
+    <View
+      allowInset
+      backgroundColor="caribbeanGreen"
+      flex={1}
+      justifyContent="flex-end">
+      {/* header */}
+      <View flex={1} justifyContent="center" alignItems="center">
+        <Text fontSize={30} fontWeight="bold" color="white">
+          New password
+        </Text>
+      </View>
+
+      {/* content */}
+      <View
+        height={'60%'}
+        width={'100%'}
+        backgroundColor="honeydew"
+        borderTopLeftRadius={55}
+        borderTopRightRadius={55}
+        paddingHorizontal={37}
+        paddingVertical={87}
+        gap={30}>
+        {/* Form */}
+        <View gap={16}>
+          <View gap={5}>
+            <Text paddingLeft={15}>Password</Text>
+            <Input
+              backgroundColor="lightGreen"
+              placeholderTextColor="cyprus"
+              placeholder="Enter password..."
+              secureTextEntry={true}
+            />
+          </View>
+
+          <View gap={5}>
+            <Text paddingLeft={15}>Confirm Password</Text>
+            <Input
+              backgroundColor="lightGreen"
+              placeholderTextColor="cyprus"
+              placeholder="Enter password..."
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+
+        {/* Footer */}
+        <TouchableOpacity
+          width={207}
+          height={45}
+          alignSelf="center"
+          justifyContent="center"
+          alignItems="center"
+          backgroundColor="caribbeanGreen"
+          borderRadius={30}>
+          <Text fontSize={20} fontWeight={'bold'} color="void">
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default NewPassword;
